d66985d9cce3d131b7cf06ab43c77ae763af95da
ryuji.tamakawa <homes31285@gmail.com>
2021-08-12 13:24:01 +0000

    Second commit

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,28 +1,35 @@
 class TasksController < ApplicationController
     before_action :set_task, only: [:show, :edit, :update, :destroy]
-    
+    before_action :require_user_logged_in
+    before_action :correct_user, only: [:destroy]
+
     def index
-        @tasks = Task.all
+        @pagy, @tasks = pagy(Task.order(id: :desc), items:20)
     end 
     
-    def show
-    end 
+  def show
+    @user = User.find(params[:id])
+    @pagy, @tasks = pagy(@user.tasks.order(id: :desc))
+    counts(@user)
+  end
     
     def new
         @task = Task.new
     end 
     
     def create 
-        @task = Task.new(task_params)
+        @task = current_user.tasks.build(task_params)
         
         if @task.save
             flash[:success] = 'Task が正常に投稿されました'
-            redirect_to @task
+            redirect_to root_url
         else
+            @pagy, @tasks = pagy(current_user.tasks.order(id: :desc))
             flash.now[:danger] = 'Taskが投稿されませんでした'
-            render :new
+            render 'toppages/index'
         end 
     end 
+
     
     def edit
     end 
@@ -41,7 +48,7 @@ class TasksController < ApplicationController
         @task.destroy
         
         flash[:success] = 'Taskは正常に削除されました'
-        redirect_to tasks_url
+        redirect_back(fallback_location: root_path)
     end
     
      private
@@ -53,4 +60,21 @@ class TasksController < ApplicationController
      def task_params
          params.require(:task).permit(:content, :status)
      end
+     
+   def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+        redirect_to root_url
+    end
+   end
+   
+  def require_user_logged_in
+    unless logged_in?
+      redirect_to login_url
+    end
+  end
+
+  def counts(user)
+    @count_microposts = user.microposts.count
+  end
 end
---
+++ app/controllers/toppages_controller.rb
@@ -0,0 +1,9 @@
+class ToppagesController < ApplicationController
+  def index
+    if logged_in?
+      @task = current_user.tasks.build
+      
+      @pagy, @tasks = pagy(current_user.tasks.order(id: :desc))
+    end
+  end
+end
